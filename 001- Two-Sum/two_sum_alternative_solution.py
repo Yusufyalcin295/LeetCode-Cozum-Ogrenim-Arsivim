489beca8bb763ee4c4924b5059465f2cc693b32a
Yusuf Yalçın <ysfyalcin295@gmail.com>
2025-11-19 17:03:08 +0300

    Two sum aciklamalari ve cozumleri eklendi

---
+++ 001- Two-Sum/two_sum_alternative_solution.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Değer : İndeks şeklinde saklayacağımız boş bir sözlük
+        seen = {} 
+        
+        for i, n in enumerate(nums):
+            diff = target - n
+            # Aradığımız fark sözlükte var mı?
+            if diff in seen:
+                # Varsa, o sayının indeksi ve şu anki indeks cevaptır
+                return [seen[diff], i]
+            # Yoksa, şu anki sayıyı ve indeksini sözlüğe ekle
+            seen[n] = i
---
+++ 001- Two-Sum/two_sum_solution.py
@@ -0,0 +1,15 @@
+# Soru Linki: https://leetcode.com/problems/two-sum/
+# Konu: Two Sum (Easy)
+
+from typing import List 
+
+
+#İç içe iki dögü kuruyoruz ilk sayıyı alıp geri kalan tüm sayılarla toplayıp 
+#hedefimize eşitse indeksleri dödürüyoruz değilse ikinci sayıyla aynı işlemleri yapıyoruz bulana kadar
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i]+nums[j] == target:
+                    return [i,j]
+
